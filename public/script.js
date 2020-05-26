4541b6ded653b80c976250791301658b9e2ff9f1
tt59317 <57324886+tt59317@users.noreply.github.com>
2020-05-26 11:14:08 +0300

    update css

--- public/script.js
+++ public/script.js
@@ -118,7 +118,7 @@ const setTime = () => {
 }
 
 const pad = val => {
-   let valString = val + '';
+   let valString =val + '';
    if(valString.length < 2){
       return 0 + valString;
    } else {
